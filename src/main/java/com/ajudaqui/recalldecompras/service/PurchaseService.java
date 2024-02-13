f189dff8fc0153deba568d753fae8eed8d6568b8
gustavo91010 <gustavo910@gmail.com>
2024-02-13 00:12:39 -0300

    celan code

--- src/main/java/com/ajudaqui/recalldecompras/service/PurchaseService.java
+++ src/main/java/com/ajudaqui/recalldecompras/service/PurchaseService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ajudaqui.recalldecompras.entity.Purchase;
+import com.ajudaqui.recalldecompras.entity.PurchaseItem;
 import com.ajudaqui.recalldecompras.entity.Users;
 import com.ajudaqui.recalldecompras.exception.NotFoundEntityException;
 import com.ajudaqui.recalldecompras.repository.PurchaseRepository;
@@ -24,28 +25,30 @@ public class PurchaseService {
 	public List<Purchase> newPurchase(Long userId) {
 		Users user = usersService.findById(userId);
 		Purchase purchase = new Purchase(user);
-		purchase= purchaseRepository.save(purchase);
-		
+		purchase = purchaseRepository.save(purchase);
+
 		List<Purchase> purchases = findAllByUsers(userId);
-		
+
 		purchases.add(purchase);
-		
+
 		return purchases;
 
 	}
 
+	// clonagem do historico de compras
 	public Purchase shoppingClone(Long userId, Long purchaseId) {
 		if (!purchasePertenceUser(userId, purchaseId)) {
 			throw new NotFoundEntityException("Compra não localizada.");
 		}
 		Purchase purchase = findById(purchaseId);
 
+//		ele adiciona os itens da utima lista nessa lista
 		purchase.getItems().forEach(item -> item.getProduct().setPrice(BigDecimal.ZERO));
 		return purchase;
 	}
 
-	public Purchase findById(Long purchaseID) {
-		Optional<Purchase> purchase = purchaseRepository.findById(purchaseID);
+	public Purchase findById(Long id) {
+		Optional<Purchase> purchase = purchaseRepository.findById(id);
 		if (purchase.isEmpty()) {
 			throw new NotFoundEntityException("Compra não localizada.");
 		}
@@ -62,15 +65,23 @@ public class PurchaseService {
 		return purchaseRepository.findAllByUsers(userId);
 
 	}
-	
+
+
 	public Purchase attPurchase(Purchase purchase) {
-		purchase=	purchaseRepository.save(purchase);
+		purchase = purchaseRepository.save(purchase);
 		return purchase;
-		
+
 	}
 
-//	 traz o histórico de quantidade
-//		- histórico de preço
-//		- media de quantidade
-//		- média de preços 
+	public BigDecimal totalPrice(Long id) {
+		Purchase purchase = findById(id);
+
+		BigDecimal total = BigDecimal.ZERO;
+
+		for (PurchaseItem item : purchase.getItems()) {
+			total = total.add(item.getTotalValue());
+		}
+		return total;
+	}
+
 }
